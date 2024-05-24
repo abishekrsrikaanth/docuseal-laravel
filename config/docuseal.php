2004ac22768c8f1f627fb82ceaf5ed684e0842ea
Abishek R Srikaanth <abishek@technicaliti.me>
2024-05-24 21:52:11 +0530

    Fixing the API URL

--- config/docuseal.php
+++ config/docuseal.php
@@ -3,7 +3,5 @@
 // config for Docuseal/Docuseal
 return [
     'api_token' => env('DOCUSEAL_API_TOKEN'),
-    'base_url' => env('DOCUSEAL_BASE_URL'),
-    'api_url' => env('DOCUSEAL_SELF_HOSTED', false) ? 'https://api.docuseal.co' : env('DOCUSEAL_URL').'/api',
-    'self_hosted' => env('DOCUSEAL_SELF_HOSTED', false),
+    'api_url' => env('DOCUSEAL_API_URL', 'https://api.docuseal.co'),
 ];
